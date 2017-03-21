c1a228d2b99121069daabc6a5f4ff5f709dc6aa9
Daniel Beardsley <daniel@ifixit.com>
2017-03-21 16:19:36 -0700

    bb: debug logging - add to filter-issues.js

--- filter-issues.js
+++ filter-issues.js
@@ -1,6 +1,11 @@
+var debug = require('debug')('backlog:filter');
+
 module.exports = function removeIssuesWitoutPriority(issues) {
-   return issues.filter(function(issue) {
+   debug("filtering %s issues, removing those without priority", issues.length);
+   var output = issues.filter(function(issue) {
       return issue.hasPriority();
    });
-}
+   debug("Removed %s issues without priorities", issues.length - output.length);
+   return output;
+};
 
