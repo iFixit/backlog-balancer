4d90d3666d1987b665ef2441a737268d84d28160
Daniel Beardsley <daniel@ifixit.com>
2017-03-23 11:34:13 -0700

    bb: add config example and use numBuckets

--- balance.js
+++ balance.js
@@ -1,11 +1,14 @@
 var db = require('./db.js');
 var debug = require('debug')('backlog');
+var config = require('./config.js');
 
 db.getIssuesAndLabelRows()
 .then(db.createIssueObjects)
 .then(require('./filter-issues.js'))
 .then(require('./sort-issues.js'))
-.then(require('./bucketize-issues.js'))
+.then(function(issues) {
+   return require('./bucketize-issues.js')(issues, config.numBuckets);
+})
 .then(require('./apply-labels.js')(require('./github.js')))
 .done(function () {
    debug("Balancing complete");
---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+   owner: 'github-repo-owner-name',
+   repo: 'github-repo-name',
+   github: {
+      token: 'github user token'
+   },
+   numBuckets: 9
+};
