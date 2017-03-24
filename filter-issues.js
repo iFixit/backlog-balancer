19b959b32def3cdf06aac4ecc87f05808bd73f11
Daniel Beardsley <daniel@ifixit.com>
2017-03-24 11:17:20 -0700

    bb: filter-issues: improve function name
    and make debug have grammers.

--- filter-issues.js
+++ filter-issues.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('backlog:filter');
 
-module.exports = function removeIssuesWitoutPriority(issues) {
-   debug("filtering %s issues, removing those without priority", issues.length);
+module.exports = function removeIssuesWithoutPriority(issues) {
+   debug("Filtering %s issues; removing those without priority", issues.length);
    var output = issues.filter(function(issue) {
       return issue.hasPriority();
    });
